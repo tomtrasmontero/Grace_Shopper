d84380e96288ca63b95d705da6efc781f03dda8d
tomtrasmontero <tomtrasmontero@gmail.com>
2016-10-17 00:55:06 -0400

    product details basic with reviews complete

--- server/app/routes/product/index.js
+++ server/app/routes/product/index.js
@@ -1,6 +1,7 @@
 'use strict';
 var router = require('express').Router();
 var Instrument = require('../../../db').models.instrument;
+var Review = require('../../../db').models.review;
 module.exports = router;
 
 router.get('/', function(req,res,next){
@@ -8,5 +9,19 @@ router.get('/', function(req,res,next){
 	.then(function(instruments){
 		res.send(instruments);
 	});
+});
+
+router.get('/:id', function(req,res,next){
+	return Instrument.findOne({
+		where:{
+			id: req.params.id
+		},
+		include:[{
+			model: Review
+		}]
+	})
+	.then(function(result){
+		res.send(result);
+	})
 });
 
