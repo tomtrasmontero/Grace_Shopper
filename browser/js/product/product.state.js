fbd98522f2dac6b3667974d0f42b5b84fcda27f3
Will Livengood <wlivengo@gmail.com>
2016-10-20 13:14:52 -0400

    removed unecessary semi-colon in products state

--- browser/js/product/product.state.js
+++ browser/js/product/product.state.js
@@ -19,7 +19,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 				return productFactory.getProduct($stateParams.id)
 			}
 		}
-	});
+	})
 	.state('productMgmt', {
 		url: '/product/mgmt',
 		templateUrl: 'js/product/product.mgmt.html',
