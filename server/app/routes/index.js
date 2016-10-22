031d1868a633d7601566ae3091642443ceccb887
hekod777 <lifetimeinc@gmail.com>
2016-10-21 23:02:44 -0400

    added cart routes
    added cart routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -5,6 +5,7 @@ module.exports = router;
 router.use('/users', require('./users'));
 router.use('/product', require('./product'));
 router.use('/order', require('./order'));
+router.use('/cart', require('./cart'));
 
 // Make sure this is after all of
 // the registered routes!
