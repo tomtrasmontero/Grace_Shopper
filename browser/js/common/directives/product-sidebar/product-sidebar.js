cd56523462d760dc661b160db361515de0ac9d3e
tomtrasmontero <tomtrasmontero@gmail.com>
2016-10-27 18:16:34 -0400

    updated product management to add items

--- browser/js/common/directives/product-sidebar/product-sidebar.js
+++ browser/js/common/directives/product-sidebar/product-sidebar.js
@@ -1,7 +1,12 @@
 app.directive('productSidebar', function(){
 	return {
 		restrict: 'E',
-		scope:{},
-		templateUrl: 'js/common/directives/product-sidebar/product-sidebar.html'		
+		templateUrl: 'js/common/directives/product-sidebar/product-sidebar.html',
+		controller: function($scope){
+			$scope.search = function(item){
+				return (angular.lowercase(item.description).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
+				angular.lowercase(item.title).indexOf(angular.lowercase($scope.query) || '') !== -1 );
+			};
+		}
 	}
 });
--- browser/js/product/product.ctrl.js
+++ browser/js/product/product.ctrl.js
@@ -2,10 +2,6 @@
 app.controller('productCtrl', function($scope,productFactory,products){
 	$scope.products = products;
 	
-	$scope.search = function(item){
-		return (angular.lowercase(item.description).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
-			angular.lowercase(item.title).indexOf(angular.lowercase($scope.query) || '') !== -1 );
-	};
 
 });
 
@@ -17,7 +13,6 @@ app.controller('productDetailCtrl', function($scope, $stateParams,$state,product
 	$scope.product.quantity = 1;
 
 	$scope.addInstrument = function(productQty,productId){
-		console.log(productQty,productId);
 		ZCartFactory.addItem(parseInt(productId),parseInt(productQty));
 	};
 
@@ -33,6 +28,10 @@ app.controller('productMgmtCtrl', function($scope, products){
 
 	$scope.changeView = function(num){
 		$scope.itemsPerPage = num;
+	}
+
+	$scope.addNewItem = function(newProduct){
+		console.log(newProduct);
 	}
 
 });
