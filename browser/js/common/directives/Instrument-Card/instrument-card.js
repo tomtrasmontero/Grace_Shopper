dafbd6718127bf57f065270cd78ee0647095c095
tomtrasmontero <tomtrasmontero@gmail.com>
2016-10-31 21:21:22 -0400

    fixed directive to run in heroku

--- browser/js/common/directives/Instrument-Card/instrument-card.js
+++ browser/js/common/directives/Instrument-Card/instrument-card.js
@@ -4,6 +4,18 @@ app.directive('productCard', function(){
 		scope:{
 			product: '='
 		},
-		templateUrl: 'js/common/directives/instrument-card/instrument-card.html'		
+		template: "\
+		<div class='container col-xs-12 col-md-4'> \
+			<div>\
+				<h3 class='text-center'>{{product.title}}</h3> \
+				<a ui-sref="productDetail({ id: {{product.id}} })" class='thumbnail'> \
+					<img class='img-responsive' style='width:100%;height:250px;' ng-src="{{product.image[0]}}" alt='product pic'/> \
+				</a> \
+			</div> \
+			<div>	\
+				<p>{{product.description}}</p> \
+			</div> \
+		<br clear='all'> \
+		</div>"
 	}
 });
